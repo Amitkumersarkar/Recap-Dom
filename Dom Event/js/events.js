42980c1d38cb7023d0740ccd72c63b6989693812
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-04 18:27:56 +0600

    added addeventListener in js dom

--- Dom Event/js/events.js
+++ Dom Event/js/events.js
@@ -1,12 +1,12 @@
-// options 1 directly set on the html element
+// option 1 directly set on the html element
 
 
-// options 2 onclick function
+// option 2 onclick function on the html element
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
 
-// options 3 
+// option 3 
 
 const makeBlueBtn = document.getElementById('make-blue');
 makeBlueBtn.onclick = makeBlue;
@@ -15,9 +15,29 @@ function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
 
-// options 3 another
+// option 3 another
 
 const purpleBtn = document.getElementById('make-purple');
 purpleBtn.onclick = function makePurple() {
     document.body.style.backgroundColor = 'purple';
 }
+
+// option 4
+
+const pinkBtn = document.getElementById('make-pink');
+pinkBtn.addEventListener('click', makePink);
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+// another option 4
+
+const makeGreenBtn = document.getElementById('Make-Green');
+makeGreenBtn.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+})
+
+// final and most useable options 
+document.getElementById('make-skyblue').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'skyblue';
+})
